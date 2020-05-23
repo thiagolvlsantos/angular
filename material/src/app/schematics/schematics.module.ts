d450e9f1f89c0af1ecd6749f2de0fb7ce49f625b
thiagolvlsantos <thiago.lvl.santos@gmail.com>
2020-05-23 19:52:42 -0300

    Using ripple.

--- material/src/app/schematics/schematics.module.ts
+++ material/src/app/schematics/schematics.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -57,6 +58,7 @@ const modules = [
   MatGridListModule,
   MatMenuModule,
   DragDropModule,
+  MatRippleModule,
   SchematicRoutingModule
 ]
 
